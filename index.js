08fa9b321caae24cb71c8d6d2faa5d104a63f854
lilian manaa <lelian.ibraheem@gmail.com>
2022-08-31 14:23:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply;
+const num1 = 31;
+const num2 =2 ;
+const random = 3;
+const num3 = 4;
+const num4 = 34;
 
-// Write your code here
+
+multiply = num1 * num2;
+console.log(multiply);
+
+Number.isInteger(random);
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(2,4,6,10,20);
